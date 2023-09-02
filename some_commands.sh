07bb4f1a501b317c8e9cd93a0013f28198ea30fa
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2023-09-02 23:42:16 +0300

    Add files via upload

---
+++ some_commands.sh
@@ -0,0 +1,18 @@
+# making mia file and listing all files and folder (including hidden)  change working directory to it 
+mkdir .mia
+
+ls -a
+
+cd .mia 
+
+# making file.txt
+touch file.txt 
+
+# edit file.txt
+echo "hello mia" >> file.txt
+
+# print the content of file.txt
+cat file.txt
+
+# clear terminal screen
+clear
